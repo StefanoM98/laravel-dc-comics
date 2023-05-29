94e94d5ce5921f17fcd53767b8d67db2a67a81b1
StefanoM98 <smartino030@gmail.com>
2023-05-29 18:02:44 +0200

    Add: new item in database

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,11 +7,11 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="title">
+                <input type="text" class="form-control" id="title" name="title">
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Scrivi la descrizione</label>
-                <textarea class="form-control" id="description" rows="3"></textarea>
+                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Metti l'url dell'immagine</label>
@@ -19,18 +19,19 @@
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Inserisci il prezzo di listino</label>
-                <input type="price" class="form-control" id="price">
+                <input type="price" class="form-control" id="price" name="price">
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Inserisci la serie di appartenenza</label>
-                <input type="text" class="form-control" id="series">
+                <input type="text" class="form-control" id="series" name="series">
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Inserisci la data di inizio vendite</label>
-                <input type="text" class="form-control" id="sale_date">
+                <input type="text" class="form-control" id="sale_date" name="sale_date">
             </div>
             <div class="mb-3">
-                <select class="form-select" aria-label="Scegli il tipo di fumetto">
+                <label for="type">Tipo di fumetto</label>
+                <select class="form-select" name="type" id="type">
                     <option selected>Scegli il tipo di fumetto</option>
                     <option value="comic_book">Comic Book</option>
                     <option value="graphic_novel">Graphic Novel</option>
